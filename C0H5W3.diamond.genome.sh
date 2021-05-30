51ec324dc745e0c5c334c2c80ead0073281515b4
jonbelmar <83778145+jonbelmar@users.noreply.github.com>
2021-05-30 18:00:57 -0400

    Update C0H5W3.diamond.genome.sh

--- C0H5W3.diamond.genome.sh
+++ C0H5W3.diamond.genome.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in GCF_000001215.4_Release_6_plus_ISO1_MT_protein GCF_000001405.39_GRCh38.p13_protein GCF_000001635.27_GRCm39_protein GCF_000001735.4_TAIR10.1_protein GCF_000002035.6_GRCz11_protein GCF_000002315.6_GRCg6a_protein GCF_000002985.6_WBcel235_protein GCF_000090745.1_AnoCar2.0_protein GCF_000146045.2_R64_protein GCF_000327385.1_Helro1_protein GCF_001663975.1_Xenopus_laevis_v2_protein GCF_003254395.2_Amel_HAv3.1_protein GCF_004143615.1_amil_sf_1.1_protein GCF_005508785.1_pea_aphid_22Mar2018_4r6ur_protein; do
+for i in GCA_001636745.1_NOR_1.0_protein GCA_002818225.1_ASM281822v1_protein GCA_012184315.1_ASM1218431v1_protein GCF_000002945.1_ASM294v2_protein GCF_000091045.1_ASM9104v1_protein GCF_000143535.2_ASM14353v4_protein GCF_000146045.2_R64_protein GCF_000146945.2_ASM14694v2_protein GCF_000149335.2_ASM14933v2_protein GCF_000149555.1_ASM14955v1_protein GCF_000149845.2_SJ5_protein GCF_000150505.1_SO6_protein GCF_011074885.1_ASM1107488v2_protein GCF_900079805.1_Fusarium_fujikuroi_IMI58289_V2_protein; do
 
 diamond makedb --in $i.faa --db ${i}.dmnd;
 
-diamond blastp --query C0H5W3.fasta --db $i.dmnd --more-sensitive --evalue 0.01 --outfmt 6 qseqid sseqid bitscore evalue qcovhsp pident >output_diamond6{$i}.bls;
+diamond blastp --query C0H5W3.fasta --db $i.dmnd --more-sensitive --evalue 0.01 --outfmt 6 qseqid sseqid bitscore evalue qcovhsp pident >output_diamond{$i}.bls;
 
 echo $i;
 echo "qseqid                 sseqid           bitscore evalue qcovhsp pident";
-echo | head output_diamond6{$i}.bls;
+echo | head output_diamond{$i}.bls;
 
-done >tabla.txt
+done >tabla.genomas.hongos.txt
